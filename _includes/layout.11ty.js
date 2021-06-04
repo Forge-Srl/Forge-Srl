c9ff1d7e7d07d91c6d7dd0e48408c5803bfea977
Tommaso Bossi <tommaso.bossi@forge.srl>
2021-06-04 09:36:59 +0200

    feat: use official Tag Manager of forge.srl

--- _includes/layout.11ty.js
+++ _includes/layout.11ty.js
@@ -34,12 +34,12 @@ module.exports = function(data) {
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-KGHFS8F');</script>
+                })(window,document,'script','dataLayer','GTM-P8LVR4Q');</script>
                 <!-- End Google Tag Manager -->
             </head>
             <body>
                 <!-- Google Tag Manager (noscript) -->
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGHFS8F"
+                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8LVR4Q"
                 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                 <!-- End Google Tag Manager (noscript) -->
                 ${header.call(this, data)}
