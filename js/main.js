9ca3ddb8ab620dc98dc3b03b97c6a896601f2a05
Tommaso Bossi <tommaso.bossi@forge.srl>
2021-06-14 16:02:24 +0200

    feat: improve glider interaction

--- js/main.js
+++ js/main.js
@@ -16,19 +16,46 @@ const initSliders = () => {
         const slidesCount = slider.track.childElementCount
         let slideTimeout = null
         let nextIndex = 1
+        let prevX = null
 
-        const slideHandler = () => {
+        const nextSlideHandler = () => {
             if (nextIndex >= slidesCount) {
                 nextIndex = 0
             }
             slider.scrollItem(nextIndex++)
         }
-        const slide = () => { slideTimeout = setTimeout(slideHandler, milliseconds) }
+        const slide = () => { slideTimeout = window.setTimeout(nextSlideHandler, milliseconds) }
 
-        slider.ele.addEventListener('glider-animated', function() {
-            window.clearInterval(slideTimeout)
+        const handleDragStart = currentX => {
+            window.clearTimeout(slideTimeout)
+            prevX = currentX
+        }
+        const handleDragEnd = currentX => {
+            const showNext = currentX - prevX < 0
+            prevX = null
+
+            if (showNext && nextIndex < slidesCount) {
+                nextSlideHandler()
+            } else if (!showNext && nextIndex > 0) {
+                nextIndex -= 2
+                nextSlideHandler()
+            }
+
+            slide()
+        }
+
+        slider.ele.addEventListener('glider-animated', () => {
+            window.clearTimeout(slideTimeout)
             slide()
         })
+        slider.ele.addEventListener('glider-slide-visible', event => {
+            // Sync dots navigation with auto-slide and drag
+            nextIndex = event.detail.slide + 1
+        })
+        slider.track.addEventListener('mousedown', event => handleDragStart(event.clientX))
+        slider.track.addEventListener('touchstart', event => handleDragStart(event.touches[0].clientX))
+        slider.track.addEventListener('mouseup', event => handleDragEnd(event.clientX))
+        slider.track.addEventListener('touchend', event => handleDragEnd(event.changedTouches[0].clientX))
 
         slide()
     }
@@ -38,7 +65,9 @@ const initSliders = () => {
             slidesToShow: 1,
             slidesToScroll: 'auto',
             duration: .5,
-            dots: '.dots'
+            dots: '.dots',
+            draggable: true,
+            dragVelocity: 0.5,
         })
 
         autoSlide(slider, 5000)
