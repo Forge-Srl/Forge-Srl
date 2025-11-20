7148b18c1841ccd59d9887f68a2b7c4908200eb8
Tommaso Bossi <tommaso.bossi@forge.srl>
2025-11-20 14:41:07 +0100

    WIP easter egging...

---
+++ assets/js/rabbit-hole.js
@@ -0,0 +1,70 @@
+(() => {
+    class RabbitHole {
+        #containerElement;
+
+        constructor() {
+            const css = document.createElement('link')
+            css.rel = 'stylesheet'
+            css.type = 'text/css'
+            css.href = '/assets/css/rabbit-hole.css'
+            document.head.appendChild(css)
+
+            const overlayElement = document.createElement('div')
+            overlayElement.classList.add('rabbit-hole-overlay')
+
+            const containerElement = document.createElement('div')
+            containerElement.classList.add('box')
+            containerElement.style = 'width: var(--terminal-width); box-sizing: content-box;'
+            overlayElement.appendChild(containerElement)
+            document.body.appendChild(overlayElement)
+            this.#containerElement = containerElement
+        }
+
+        start() {
+            window.takePill = (color) => {
+                // Since this function is exposed to the curious we allow more definitions of blue and red other than
+                // the one used in the code itself.
+                switch (color) {
+                    case 'blue':
+                    case '#0000ff':
+                    case '#0000FF':
+                        window.location.href = '/'
+                        break
+                    case 'red':
+                    case '#ff0000':
+                    case '#FF0000':
+                        this.#afterRedPill()
+                        break
+                    default:
+                        console.error(`Cannot take ${color} pill`)
+                }
+            }
+            this.#containerElement.innerHTML = `<blockquote>
+                    <p>
+                    Nessuno di noi è in grado, purtroppo, di descrivere Forge agli altri. Dovrai scoprire con i tuoi occhi che cos'è.<br>
+                    È la tua ultima occasione, se rinunci non ne avrai altre.
+                    </p>
+                    <ul>
+                    <li><strong>Pillola azzurra</strong>, fine della storia: domani ti sveglierai in camera tua, e crederai a quello che vorrai.</li>
+                    <li><strong>Pillola rossa</strong>, resti nel paese delle meraviglie, e vedrai quant'è profonda la tana del Bianconiglio.</li>
+                    </ul>
+                    <br>
+                    <p>Ti sto offrendo solo la verità, ricordalo. Niente di più.</p>
+                </blockquote>
+                <br>
+                <div class="group-2 gap-3">
+                    <button type="button" class="pill red" onclick="takePill('red')">Pillola rossa</button>
+                    <button type="button" class="pill blue" onclick="takePill('blue')">Pillola azzurra</button>
+                <div>`
+        }
+
+        #afterRedPill() {
+            // TODO
+            this.#containerElement.innerHTML = ''
+            console.log('TODO')
+        }
+    }
+
+    const rabbitHole = new RabbitHole()
+    window.whiteRabbit.onFollowLoaded(rabbitHole)
+})()
--- assets/js/start.js
+++ assets/js/start.js
@@ -1,7 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log('%c{FORGE}', 'font-size: 6em; color: #F08819')
-    console.log('Cosa stai facendo qui? Non c\'è nulla da vedere!')
+const advertiser = (lines) => {
+    const maxLength = Math.max(...lines.map(l => l.length))
     console.log('')
-    console.log('Vuoi lavorare con noi?')
-    console.log(new URL('/lavora-con-noi', window.location.href).toString())
+    console.log(`%c+${'-'.repeat(maxLength+2)}+`, 'font-family: monospace;')
+    lines.forEach(line => {
+        console.log(`%c| ${line.padEnd(maxLength, ' ')} |`, 'font-family: monospace;')
+    })
+    console.log(`%c+${'-'.repeat(maxLength+2)}+`, 'font-family: monospace;')
+    console.log('')
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('%c{FORGE}', 'font-size: 6em; font-family: monospace; color: #F08819')
+    advertiser([
+        'Vuoi lavorare con noi?',
+        new URL('/lavora-con-noi', window.location.href).toString(),
+    ])
+    window.whiteRabbit = {
+        follow: () => {
+            console.log('Loading...')
+            const rabbitHole = document.createElement('script')
+            rabbitHole.src = new URL('/assets/js/rabbit-hole.js', window.location.href).toString()
+            document.body.append(rabbitHole)
+        },
+        onFollowLoaded: (rabbitHole) => {
+            rabbitHole.start()
+            console.log('Welcome!')
+        }
+    }
+    console.log('%cFollow the `whiteRabbit` out of the `window`...', 'font-family: monospace; font-style: italic;')
 })
